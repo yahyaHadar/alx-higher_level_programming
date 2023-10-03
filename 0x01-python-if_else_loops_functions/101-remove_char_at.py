d7de48567b4b3f35d1c2c9460fcd9a1caaa9d587
yahyaHadar <yahyahadar@gmail.com>
2023-10-03 16:36:23 -0700

    commit

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    new = ''
-    i = 0
-    for c in str:
+    newstrr = ""
+    for i, c in enumerate(str):
         if i != n:
-            new += str[i]
-        i += 1
-        return new
+            newstrr += c
+        return newstrr
