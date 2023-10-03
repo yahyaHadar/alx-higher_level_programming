069668f7cb56c1b9ea2de432a49e0ce5888d4983
yahyaHadar <yahyahadar@gmail.com>
2023-10-03 10:43:04 -0700

    commit

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+print(f"{number} is", end=" ")
+
+if number > 0:
+    print("positive")
+elif number == 0:
+    print("zero")
+else:
+    print("negative")
