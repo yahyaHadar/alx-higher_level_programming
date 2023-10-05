9c9981208ae280b050a47567bcf3ec5461c418b2
yahyaHadar <yahyahadar@gmail.com>
2023-10-05 07:34:14 -0700

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    num = len(sys.argv)
-    if num == 1:
-        print("{} arguments.".format(num - 1))
-    elif num == 2:
-        print("{} argument:".format(num - 1))
+    num = len(sys.argv) -1
+    if num == 0:
+        print(" 0 arguments")
+    elif num == 1:
+        print("1 argument")
     else:
-        print("{} arguments:".format(num - 1))
+        print("{} arguments:".format(num))
 
-
-    for i in range(1, num):
-        print("{}: {}".format(i, sys.argv[i]))
+    for i in range(num):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
