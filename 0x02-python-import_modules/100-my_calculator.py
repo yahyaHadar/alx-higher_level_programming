30134b418fe26aade96a6ccf9e5a299bbea3d7ed
yahyaHadar <yahyahadar@gmail.com>
2023-10-05 07:55:15 -0700

    m

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         b = int(sys.argv[3])
         if op == '+':
             print("{} {} {} = {}".format(a, op, b, add(a, b)))
-        elif o == '-':
+        elif op == '-':
             print("{} {} {} = {}".format(a, op, b, sub(a, b)))
         elif op == '*':
             print("{} {} {} = {}".format(a, op, b, mul(a, b)))
