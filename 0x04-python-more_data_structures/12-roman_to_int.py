cf42ae3ecaf625f346e4988088062b75a3f7d72d
yahyaHadar <yahyahadar@gmail.com>
2023-10-11 15:18:08 +0100

    commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
     roman_dict = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
     }
     decs = [roman_dict[x] for x in roman_string]
     output = 0
