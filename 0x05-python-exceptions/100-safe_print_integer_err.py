19ed4344846078972dc312ed7929214df4c1c302
yahyaHadar <yahyahadar@gmail.com>
2023-10-23 07:47:10 -0700

    commit

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return False
+    else:
+        return True
